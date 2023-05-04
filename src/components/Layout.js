a4f0c617f0e2d058800814e476cb6848f55d8e8a
ajha-digi <ajha@digisprint.com>
2023-05-04 16:09:35 +0530

    Added responsive navbar menu.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import NavBar from './Navbar';
+
 
 function Layout() {
     return (
-        <div>
-            <header>
-                this is heading
-            </header>
-            <main>
-                <Outlet />
-            </main>
-            <footer>
-                this is footer
-            </footer>
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="xl">
+                <Box sx={{height: '100vh' }}>
+                    <header>
+                        <NavBar />
+                    </header>
+                    <main>
+                        <Outlet />
+                    </main>
+                    <footer>
+                        this is footer
+                    </footer>
+                </Box>
+            </Container>
+        </React.Fragment>
     )
 }
 
